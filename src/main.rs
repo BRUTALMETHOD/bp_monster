c53b8ef1c5366da56c497e6f0b4a80e4e0e9ce70
BRUTALMETHOD <brutalmethod@users.noreply.github.com>
2023-06-15 02:43:20 +0800

    WIP, neet to overwrite activity method to custom

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use std::env;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
 use std::time::Duration;
 
 use serenity::async_trait;
 use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::{CommandResult, StandardFramework};
 use serenity::model::channel::Message;
-use serenity::model::gateway::Ready;
-use serenity::model::id::GuildId;
+use serenity::model::gateway::{Activity, Ready};
+//use serenity::model::id::GuildId;
 use serenity::prelude::*;
 
 use reqwest;
@@ -16,6 +15,11 @@ use reqwest;
 #[group]
 #[commands(ping)]
 struct General;
+
+enum LauncherStatus {
+    Up,
+    Down,
+}
 
 struct Handler {
     is_loop_running: AtomicBool,
@@ -35,7 +39,7 @@ impl EventHandler for Handler {
             }
         }
     }
-    async fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
         // We need to check that the loop is not already running when this event triggers,
         // as this event triggers every time the bot enters or leaves a guild, along every time the
@@ -53,7 +57,16 @@ impl EventHandler for Handler {
                     // We clone Context again here, because Arc is owned, so it moves to the
                     // new function.
                     //log_system_load(Arc::clone(&ctx1)).await;
-                    let _ = get_launcher_status().await;
+                    match get_launcher_status().await.unwrap() {
+                        LauncherStatus::Up => {
+                            ctx.set_activity(Activity::playing("Blue Protocol is Up!"))
+                                .await;
+                        }
+                        LauncherStatus::Down => {
+                            ctx.set_activity(Activity::playing("Blue Protocol is Down!"))
+                                .await;
+                        }
+                    }
                     tokio::time::sleep(Duration::from_secs(10)).await;
                 }
             });
@@ -64,7 +77,7 @@ impl EventHandler for Handler {
     }
 }
 
-async fn get_launcher_status() -> Result<(), reqwest::Error> {
+async fn get_launcher_status() -> Result<LauncherStatus, reqwest::Error> {
     // check status at url: http://api-bnolauncher.bandainamco-ol.jp
     // maintenance msg : {
     //    "Message": "現在メンテナンス中です。",
@@ -73,13 +86,16 @@ async fn get_launcher_status() -> Result<(), reqwest::Error> {
     println!("Fetching url..");
     let resp = reqwest::get("https://api-bnolauncher.bandainamco-ol.jp").await?;
     eprintln!("{:#?}", resp);
-    if resp.status() == 503 {
-        eprintln!("Launcher is under maintenance: {:#?}", resp);
+    match resp.status() {
+        reqwest::StatusCode::OK => {
+            eprintln!("Launcher is OK!: {:#?}", resp);
+            Ok(LauncherStatus::Up)
+        }
+        _ => {
+            eprintln!("Launcher is under maintenance: {:#?}", resp);
+            Ok(LauncherStatus::Down)
+        }
     }
-    if resp.status() == 200 {
-        eprintln!("Launcher is OK!: {:#?}", resp);
-    }
-    Ok(())
 }
 
 #[tokio::main]
